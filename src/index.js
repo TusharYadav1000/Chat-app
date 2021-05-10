c2fd97be9105a3da1623f8dac478a446333038b0
tushar <tusharyada85@gmail.com>
2021-05-11 02:53:17 +0530

    Making client side connection using socket.io

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
 
 const port = process.env.PORT || 5000
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
 
-app.listen(port, () => { console.log(`Server is running on ${port}`) })
+
+
+server.listen(port, () => { console.log(`Server is running on ${port}`) })
